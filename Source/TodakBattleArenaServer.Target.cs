22db14071987f454bc97ce93ad2822c99297a3d6
razilah <razisbetterthanyou@gmail.com>
2021-04-20 14:07:39 +0800

    Update gitignore

--- Source/TodakBattleArenaServer.Target.cs
+++ Source/TodakBattleArenaServer.Target.cs
@@ -3,12 +3,13 @@
 using UnrealBuildTool;
 using System.Collections.Generic;
 
+[SupportedPlatforms(UnrealPlatformClass.Server)]
 public class TodakBattleArenaServerTarget : TargetRules
 {
-	public TodakBattleArenaServerTarget(TargetInfo Target) : base(Target)
+    public TodakBattleArenaServerTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Server;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		BuildEnvironment = TargetBuildEnvironment.Shared;
 		ExtraModuleNames.Add("TodakBattleArena");
 	}
 }
