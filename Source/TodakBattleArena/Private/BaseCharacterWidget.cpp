2952e2a0ff3e6f772334fc5011e011c082839ae0
razilah <razisbetterthanyou@gmail.com>
2021-05-17 11:08:57 +0800

    Update BaseCharacterWidget.cpp

--- Source/TodakBattleArena/Private/BaseCharacterWidget.cpp
+++ Source/TodakBattleArena/Private/BaseCharacterWidget.cpp
@@ -16,22 +16,22 @@ void UBaseCharacterWidget::NativeConstruct()
 	Super::NativeConstruct();
 }
 
-void UBaseCharacterWidget::ChangeProgressBarValue(AActor* currPlayer, float currVal, int MaxVal, float& currPercentage)
-{
-	//Update energy after action on progress bar
-	//UE_LOG(LogTemp, Warning, TEXT("Energy Remains: %f"), currPlayer->playerEnergy);
-
-	//currPlayer->EnergyPercentage = UGestureInputsFunctions::UpdateProgressBarComponent(this, "EnergyBar", "EnergyText", "Energy", "Energy", currPlayer->playerEnergy, currPlayer->MaxEnergy);
-
-	/*if (currWidget->GetOwningPlayerPawn() == currPlayer)
-	{
-		currPercentage = UGestureInputsFunctions::UpdateProgressBarComponent(currWidget, "EnergyBar", "EnergyText", "Energy", "Energy", currVal, MaxVal);
-		UE_LOG(LogTemp, Warning, TEXT("Energy Remains: %f"), currPercentage);
-	}*/
-	
-	if (GetOwningPlayer() != currPlayer)
-	{
-		currPercentage = UGestureInputsFunctions::UpdateProgressBarComponent(this, "EnergyBar_1", "EnergyText_1", "Energy", "Energy", currVal, MaxVal);
-		UE_LOG(LogTemp, Warning, TEXT(".......Energy Remains: %f"), currPercentage);
-	}
-}
+//void UBaseCharacterWidget::ChangeProgressBarValue(AActor* currPlayer, float currVal, int MaxVal, float& currPercentage)
+//{
+//	//Update energy after action on progress bar
+//	//UE_LOG(LogTemp, Warning, TEXT("Energy Remains: %f"), currPlayer->playerEnergy);
+//
+//	//currPlayer->EnergyPercentage = UGestureInputsFunctions::UpdateProgressBarComponent(this, "EnergyBar", "EnergyText", "Energy", "Energy", currPlayer->playerEnergy, currPlayer->MaxEnergy);
+//
+//	/*if (currWidget->GetOwningPlayerPawn() == currPlayer)
+//	{
+//		currPercentage = UGestureInputsFunctions::UpdateProgressBarComponent(currWidget, "EnergyBar", "EnergyText", "Energy", "Energy", currVal, MaxVal);
+//		UE_LOG(LogTemp, Warning, TEXT("Energy Remains: %f"), currPercentage);
+//	}*/
+//	
+//	if (GetOwningPlayer() != currPlayer)
+//	{
+//		currPercentage = UGestureInputsFunctions::UpdateProgressBarComponent(this, "EnergyBar_1", "EnergyText_1", "Energy", "Energy", currVal, MaxVal);
+//		UE_LOG(LogTemp, Warning, TEXT(".......Energy Remains: %f"), currPercentage);
+//	}
+//}
